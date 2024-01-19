d60d65f5f7eba8b9ae54416552e66a45a39758ee
Amir-Rsh <amirrashidinia93@gmail.com>
2024-01-19 14:13:10 +0000

    working on routing

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,7 +3,7 @@ const { fetchUsers } = require("../models/users.model");
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await fetchUsers();
-    return res.status(200).send({ users });
+    return res.status(200).jsonp({ users });
   } catch {
     next(err);
   }
